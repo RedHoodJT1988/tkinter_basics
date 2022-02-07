551f880e09a7a915e3694a0c6da4732bea7e485d
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-02-07 13:41:34 -0600

    add main class

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from tkinter import *
 
+
+class MyApp:
+    def __init__(self, root):
+        root.title("My App")
+        root.ge("500x400")
+        root.maxsize(1000, 800)
+
+
 root = Tk()
-root.title("My App")
+MyApp(root)
 root.mainloop()
