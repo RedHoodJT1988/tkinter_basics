295c06abb859536bef536485efeb2fb3881abc8f
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-02-07 15:57:49 -0600

    label widgets

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from tkinter import *
 class MyApp:
     def __init__(self, root):
         root.title("My App")
-        root.ge("500x400")
+        root.geometry("500x400")
         root.maxsize(1000, 800)
+
+        label = Label(root, text="Some label text")
+        label.pack()
 
 
 root = Tk()
